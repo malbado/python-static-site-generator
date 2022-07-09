27926873e6ee9379d0287cf4adc52dcb232df686
malbado <mustafa_bado@yahoo.com>
2022-07-10 00:17:55 +0100

    Update site.py

---
+++ ssg/site.py
@@ -0,0 +1,17 @@
+from msilib.schema import Directory
+from pathlib import Path
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for p in self.source.rglob("*"):
+            if p.is_dir():
+                self.create_dir(p)
